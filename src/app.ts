41add6f3470a87613fd1dc1cfd840e08ff9b2880
abu-noman30 <nomannabil30@gmail.com>
2024-03-11 01:20:56 +0600

    update: logger winston added

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Application, Request, Response } from 'express';
 import userRouter from './app/modules/users/users.route';
 
 const app: Application = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +13,7 @@ app.use('/api/v1/users', userRouter);
 
 // Test route
 app.get('/', (req: Request, res: Response) => {
-	res.send('Server is running...');
+  res.send('Server is running...');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,19 @@ import chalk from 'chalk';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
+import logger from './services/Logger/logger';
 
 async function runDB() {
   try {
     const connection = await mongoose.connect(config.db.uri as string);
     if (connection) {
-      console.log(chalk.green('Connected to MongoDB Successfully!!'));
+      logger.loggerInfo.info(chalk.green('Connected to MongoDB Successfully!!'));
       app.listen(config.port, () => {
-        console.log(chalk.green('Server is running on port...:', config.port));
+        logger.loggerInfo.info(chalk.green('Server is running on port...:', config.port));
       });
     }
   } catch (err) {
-    console.log(chalk.red('Error connecting to MongoDB'));
+    logger.loggerError.error(chalk.red('Error connecting to MongoDB'));
   }
 }
 runDB();
